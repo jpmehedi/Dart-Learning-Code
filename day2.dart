7ef1cab84dab4c27b83b57ab35ab954144acd827
Mehedi Hasan メヘディ ハサン <mehedihasan.cse.jp@gmail.com>
2020-04-09 12:23:55 +0600

    insert,add

--- day2.dart
+++ day2.dart
@@ -1,27 +1,23 @@
 void main() {
-  //fixed length list
 
-  var fixedlist = new List(3);
-
-  fixedlist[0] = "Mehedi";
-  fixedlist[1] = "Mehedi";
-  fixedlist[2] = "Mehedi";
-
-  print(fixedlist);
-
-  //growable list
+  var list = new List();
   var mylist = new List();
+  list.add("Mehedi");
+  list.add("Nirob");
+  mylist.addAll(['Mehedi', 'Hasan', 'Nirob', 'Morshed']);
 
-  mylist.add("Mehedi");
-  mylist.add("Hasan");
-  mylist.add("Nirob");
-  mylist.add("Sojol");
+  print(list);
+  print(mylist);
 
-  mylist.remove("Mehedi");
-  print(mylist.length);
-  print(mylist.last);
-  print(mylist.isEmpty);
-  print(mylist.isNotEmpty);
-  print(mylist.first);
-  print(mylist.reversed);
+  var insertList = new List();
+  var insertListAll = new List();
+
+  insertList.insert(0, "A");
+  insertList.insert(1, "B");
+  insertListAll.insertAll(0, ["a", "b", "c"]);
+
+  print(insertList);
+  print(insertListAll);
+
+  
 }
