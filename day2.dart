7823df68249d25efe0730523130d3fc37157dd00
Mehedi Hasan メヘディ ハサン <mehedihasan.cse.jp@gmail.com>
2020-04-09 12:02:00 +0600

    list

--- day2.dart
+++ day2.dart
@@ -1,4 +1,15 @@
 void main() {
+  //fixed length list
+
+  var fixedlist = new List(3);
+
+  fixedlist[0] = "Mehedi";
+  fixedlist[1] = "Mehedi";
+  fixedlist[2] = "Mehedi";
+
+  print(fixedlist);
+
+  //growable list
   var mylist = new List();
 
   mylist.add("Mehedi");
@@ -6,7 +17,7 @@ void main() {
   mylist.add("Nirob");
   mylist.add("Sojol");
 
-  //mylist.remove("Mehedi");
+  mylist.remove("Mehedi");
   print(mylist.length);
   print(mylist.last);
   print(mylist.isEmpty);
