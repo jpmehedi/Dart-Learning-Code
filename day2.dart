e5759777ea11c39b354b9cb8da141788c691df75
Mehedi Hasan メヘディ ハサン <mehedihasan.cse.jp@gmail.com>
2020-04-09 11:48:29 +0600

    list

---
+++ day2.dart
@@ -0,0 +1,16 @@
+void main() {
+  var mylist = new List();
+
+  mylist.add("Mehedi");
+  mylist.add("Hasan");
+  mylist.add("Nirob");
+  mylist.add("Sojol");
+
+  //mylist.remove("Mehedi");
+  print(mylist.length);
+  print(mylist.last);
+  print(mylist.isEmpty);
+  print(mylist.isNotEmpty);
+  print(mylist.first);
+  print(mylist.reversed);
+}
